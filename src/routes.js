2669dc4a83486ac571b48ba33db0ccb31651d75f
Maarten Mulders <m.th.mulders@xs4all.nl>
2016-07-14 08:04:46 +0200

    This hasn't been built yet, so do not include it

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,6 @@ import App from 'pages/App/';
 import CurrentPage from 'pages/CurrentReadingsPage/';
 import HomePage from 'pages/HomePage/';
 import RecentReadingsPage from 'pages/RecentReadingsPage';
-import HistoryPage from 'pages/HistoryPage';
 
 // Mind the order of routes: first match is served
 const routes = (
@@ -19,7 +18,6 @@ const routes = (
             <Route path="/home" component={ HomePage } />
             <Route path="/current" component={ CurrentPage } />
             <Route path="/recent" component={ RecentReadingsPage } />
-            <Route path="/history" component={ HistoryPage } />
         </Route>
     </Router>
 );
