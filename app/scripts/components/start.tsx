8229fd5cdda8909c51b80e7adcf871c2e6ea8357
Maarten Mulders <m.th.mulders@xs4all.nl>
2017-07-25 20:36:44 +0200

    Some text on home screen

--- app/scripts/components/start.tsx
+++ app/scripts/components/start.tsx
@@ -1,8 +1,25 @@
+import { Divider } from 'material-ui';
 import * as React from 'react';
 
 interface StartProps {
 }
 
 export const Start = (props: StartProps) => (
-    <span>This is the Start component!</span>
+    <div>
+        <h1>Welcome to Υπερίων</h1>
+        <p>
+            Υπερίων is a web application to monitor a 'Smart Energy Meter' that follows the
+            Dutch "Smart Meter" Requirements, or DSMR for short.
+        </p>
+
+        <p>
+            Open the menu on the left to get started.
+        </p>
+        <Divider />
+
+        <p>
+            &copy; 2016 - 2017 Maarten Mulders.<br />
+            Source code is available at <a href='https://github.com/mthmulders/hyperion-web' target='_blank'>GitHub</a>.
+        </p>
+    </div>
 );
