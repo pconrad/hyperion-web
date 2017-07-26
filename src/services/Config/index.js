5127aebd181b38420e537cb54692f81382c0a2ed
Maarten Mulders <m.th.mulders@xs4all.nl>
2017-07-26 14:18:09 +0200

    Remove old code

--- src/services/Config/index.js
+++
@@ -1,8 +0,0 @@
-class Config {
-    apiLocation() {
-        const host = process.env.API_HOST;
-        return process.env.NODE_ENV === 'development' ? host : window.location.hostname;
-    }
-}
-
-export default new Config();
