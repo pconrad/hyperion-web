ca651716786f324cbb9ea8e559d8a29a08c77d6b
Maarten Mulders <m.th.mulders@xs4all.nl>
2018-03-05 21:59:42 +0100

    Make graph a little wider

--- src/recent/view.tsx
+++ src/recent/view.tsx
@@ -15,7 +15,7 @@ const RecentView: React.StatelessComponent<ViewProps> = (props) => {
     const data = props.data.map((item) => ({ name: label(item), value: item.elecCon * 1000 }));
 
     return (
-        <LineChart data={ data } height={ 350 } width={ 900 }>
+        <LineChart data={ data } height={ 350 } width={ 1200 }>
             <CartesianGrid />
             <Line type='monotone' dataKey='value' points={ [] } />
             <Tooltip viewBox={{ x: 0, y: 0, width: 0, height: 0 }} />
