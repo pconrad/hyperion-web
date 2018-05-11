cdd5155d23175beea5c145d411d32d2a06c6e0be
Maarten Mulders <m.th.mulders@xs4all.nl>
2018-05-11 09:27:59 +0200

    Containers don't do their own error handling anymore

--- src/recent/__tests__/containerSpec.tsx
+++ src/recent/__tests__/containerSpec.tsx
@@ -1,8 +1,6 @@
 import { shallow } from 'enzyme';
 import * as React from 'react';
 
-import Snackbar from 'material-ui/Snackbar';
-
 const mockRetrieveRecentReadings = jest.fn(() => Promise.resolve());
 jest.mock('../../api', () => ({ retrieveRecentReadings: mockRetrieveRecentReadings }));
 
@@ -22,22 +20,5 @@ describe('<RecentContainer />', () => {
 
         // Assert
         expect(mockRetrieveRecentReadings).toHaveBeenCalled();
-    });
-
-    xit('should retry when tapping the snackbar', () => {
-        // Arrange
-        const error = new Error('Ahw, an error');
-        mockRetrieveRecentReadings.mockImplementation(() => Promise.reject(error));
-
-        // Act
-        const container = shallow(<RecentContainer />);
-        container.setState({ ...container.state(), error });
-        const snackbarProps = container.find(Snackbar).props();
-        if (snackbarProps.onActionClick) {
-            snackbarProps.onActionClick(undefined as any);
-        }
-
-        // Assert
-        expect(mockRetrieveRecentReadings).toHaveBeenCalledTimes(2);
     });
 });
