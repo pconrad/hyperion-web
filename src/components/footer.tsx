792e6ef82c2bafd9c6bd67d531add259e119541e
Maarten Mulders <m.th.mulders@xs4all.nl>
2019-08-17 20:32:19 +0200

    Add rel="noopener noreferrer" to external link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => (
         <div className='container'>
             <span className='text-muted'>
                 &copy; 2016 - 2019 Maarten Mulders.
-                Source code is available at <a href='https://github.com/mthmulders/hyperion-web' target='_blank'>GitHub</a>.
+                Source code is available at <a href='https://github.com/mthmulders/hyperion-web' target='_blank' rel='noopener noreferrer'>GitHub</a>.
             </span>
         </div>
     </footer>
