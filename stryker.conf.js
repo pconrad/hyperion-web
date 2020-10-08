ccc6ba63a9040ed218a8fb6b9abfeb1d7ac32f5e
Maarten Mulders <mthmulders@users.noreply.github.com>
2020-10-08 14:54:48 +0200

    Upgrade to Stryker 4.0.0 (#412)
    * Upgrade to Stryker 4.0.0
    
    * Explicitly set concurrency to 2

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,31 +1,29 @@
 /**
  * @type {import('@stryker-mutator/api/core').StrykerOptions}
  */
-module.exports = function(config) {
-  config.set({
-    coverageAnalysis: 'off',
-    files: [
-      'package.json',
-      'src/**/*.ts',
-      'src/**/*.tsx',
-      'src/**/__tests__/*',
-      'stryker.conf.js',
-      'tsconfig.json',
-      'tslint.json'
-    ],
-    maxConcurrentTestRunners: 5,
-    mutate: [
-      'src/**/*.ts',
-      'src/**/*.tsx',
-      '!src/**/__tests__/*'
-    ],
-    mutator: 'typescript',
-    jest: {
-      projectType: 'create-react-app'
-    },
-    reporters: ['html', 'clear-text', 'progress', 'dashboard'],
-    testRunner: 'jest',
-    timeoutMS: 60000,
-    tsconfigFile: 'tsconfig.json',
-  });
+module.exports = {
+  checkers: ['typescript'],
+  concurrency: 2,
+  coverageAnalysis: 'off',
+  files: [
+    'package.json',
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    'src/**/__tests__/*',
+    'stryker.conf.js',
+    'tsconfig.json',
+    'tslint.json'
+  ],
+  mutate: [
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    '!src/**/__tests__/*'
+  ],
+  jest: {
+    projectType: 'create-react-app'
+  },
+  reporters: ['html', 'clear-text', 'progress', 'dashboard'],
+  testRunner: 'jest',
+  timeoutMS: 60000,
+  tsconfigFile: 'tsconfig.json',
 };
